0faa0ae4445c12d836bc37afc0735fbaf354569d
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-08-08 21:59:14 -0300

    menu mobile

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-const teste = 'ailton barreto'
-
-console.log(teste)
+function menuShow(){
+    let menuMobile = document.querySelector('.nav-mobile');
+    if (menuMobile.classList.contains('open')){
+        menuMobile.classList.remove('open');
+        document.querySelector('.menu-icon').src ="./IMG/menu-icon.png";
+    } else{
+        menuMobile.classList.add('open');
+        document.querySelector('.menu-icon').src ="./IMG/cancel.png";
+    }
+}
