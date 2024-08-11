db269f74a03d22d37a28b68ba10a081ad8472a3f
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-08-11 17:01:43 -0300

    up

--- script-produto.js
+++ script-produto.js
@@ -1,3 +1,16 @@
+
+function menuShow(){
+    let menuMobile = document.querySelector('.nav-mobile');
+    if (menuMobile.classList.contains('open')){
+        menuMobile.classList.remove('open');
+        document.querySelector('.menu-icon').src ="./IMG/menu-icon.png";
+    } else{
+        menuMobile.classList.add('open');
+        document.querySelector('.menu-icon').src ="./IMG/cancel.png";
+    }
+}
+
+
 let list = document.querySelectorAll(".item")
 
 let next = document.getElementById("next")
