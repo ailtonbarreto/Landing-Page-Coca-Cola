a6bb1c2862491ee1f3c9d5127743da17602ec0d2
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-08-26 21:10:40 -0300

    making logical login

--- script-login.js
+++ script-login.js
@@ -1,7 +1,19 @@
-function Capturar(){
-    let email = document.querySelector('.email').value;
-    console.log(email);
 
-}
+function Capturar() {
+    let email = document.querySelector('#email').value;
+    let senha = document.querySelector('#senha').value;
+  
+    if (email === "" || senha === "") {
+      alert("Digite seu usuário e senha");
+      return;
+    }
+  
+    console.log("Usuário: " + email);
+    console.log("Senha: " + senha);
+  
+    document.getElementById("email").value = "";
+    document.getElementById("senha").value = "";
+  
+    event.preventDefault();
 
-
+  }
